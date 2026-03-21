8f9de1c296266f256e237aee97477c3efb4c81d5
Federico Brachi <federico.brachi@calconsultora.com.ar>
2026-03-21 17:47:38 -0300

    feat: Add i18n for English and Spanish and implement new landing page.

--- src/views/Landing.tsx
+++ src/views/Landing.tsx
@@ -39,10 +39,10 @@ const Landing: React.FC = () => {
                 <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
                   <div className="pr-12">
                     <h1 className="text-white font-semibold text-5xl">
-                      {t('landing_titulo')}
+                      {t('landing_hero_title')}
                     </h1>
                     <p className="mt-4 text-lg text-blueGray-200">
-                      {t('landing_descripcion')}
+                      {t('landing_hero_subtitle')}
                     </p>
                   </div>
                 </div>
