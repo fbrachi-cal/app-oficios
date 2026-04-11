3ad0b90b8caa74fd483598f07c616dc3d337ef09
Federico.B <federico.brachi@bemovil.net>
2026-04-11 13:27:13 -0300

    test: cambios en vercel

--- frontend/src/views/Landing.tsx
+++ frontend/src/views/Landing.tsx
@@ -18,7 +18,7 @@ const Landing: React.FC = () => {
   const { t } = useTranslation();
   const { user } = useUser(); // 👈
   const navigate = useNavigate();
-  logger.info("USER TIPO", { tipo: user?.tipo });
+  logger.info("USER TIPO V2", { tipo: user?.tipo });
   return (
     <>
       <main>
@@ -74,41 +74,41 @@ const Landing: React.FC = () => {
 
           <section className="pb-20 bg-blueGray-200 -mt-24 relative z-20">
             <div className="container mx-auto px-4">
-              <div className="flex flex-wrap">                  
+              <div className="flex flex-wrap">
                 {user?.tipo === "cliente" ? (
                   <BuscadorProfesionales />
-                ): (
-                  
+                ) : (
+
                   <div className="w-full md:w-4/12 px-4 text-center">
-                  <div 
-                    className={`relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg ${!user ? "cursor-pointer hover:shadow-xl transition-all duration-300" : ""}`}
-                    onClick={() => {
+                    <div
+                      className={`relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg ${!user ? "cursor-pointer hover:shadow-xl transition-all duration-300" : ""}`}
+                      onClick={() => {
                         if (!user) {
-                            navigate('/auth/registro');
+                          navigate('/auth/registro');
                         }
-                    }}
-                  >
-                    <div className="px-4 py-5 flex-auto">
-                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
-                        <FaCalculator className="text-white" />
+                      }}
+                    >
+                      <div className="px-4 py-5 flex-auto">
+                        <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
+                          <FaCalculator className="text-white" />
+                        </div>
+                        <h6 className="text-xl font-semibold">{t('landing_pro_headline')}</h6>
+                        <p className="mt-2 mb-4 text-blueGray-500 font-bold">
+                          {t('landing_pro_subtitle')}
+                        </p>
                       </div>
-                      <h6 className="text-xl font-semibold">{t('landing_pro_headline')}</h6>
-                      <p className="mt-2 mb-4 text-blueGray-500 font-bold">
-                        {t('landing_pro_subtitle')}
-                      </p>
                     </div>
                   </div>
-                </div>
                 )}
                 <PanelSolicitudes />
 
                 <div className="pt-6 w-full md:w-4/12 px-4 text-center">
-                  <div 
+                  <div
                     className={`relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg ${!user ? "cursor-pointer hover:shadow-xl transition-all duration-300" : ""}`}
                     onClick={() => {
-                        if (!user) {
-                            navigate('/auth/registro');
-                        }
+                      if (!user) {
+                        navigate('/auth/registro');
+                      }
                     }}
                   >
                     <div className="px-4 py-5 flex-auto">
